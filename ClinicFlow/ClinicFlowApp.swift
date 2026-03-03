227db4dfeb1b0ab92dc4a8d3052fe99f36361e26
NesaraRahal <rahalnesara@gmail.com>
2026-03-03 13:28:00 +0530

    Init Commit

---
+++ ClinicFlow/ClinicFlowApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClinicFlowApp.swift
+//  ClinicFlow
+//
+//  Created by COBSCCOMP24.2P-053 on 2026-03-03.
+//
+
+import SwiftUI
+
+@main
+struct ClinicFlowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ClinicFlowTests/ClinicFlowTests.swift
@@ -0,0 +1,17 @@
+//
+//  ClinicFlowTests.swift
+//  ClinicFlowTests
+//
+//  Created by COBSCCOMP24.2P-053 on 2026-03-03.
+//
+
+import Testing
+@testable import ClinicFlow
+
+struct ClinicFlowTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
